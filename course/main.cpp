c4f44eafdffe83d2d32e891a72abb5833dc62d22
grant030405 <146087042+grant030405@users.noreply.github.com>
2024-05-13 14:56:08 +0800

    Update main.cpp

--- course/main.cpp
+++ course/main.cpp
@@ -1,44 +1,81 @@
 
+
 #include <iostream>
 #include <string>
 using namespace std;
+
+class Engine {
+    private:
+        int m_EngineCapacity;
+        int m_Horsepower;
+        bool m_EngineRunning;
+    public:
+        Engine(int EngineCapacity=2000, int Horsepower=200) {
+            m_EngineCapacity = EngineCapacity;
+            m_Horsepower = Horsepower;
+            m_EngineRunning = false;
+        }
+        bool EngineStart() {
+            if(!m_EngineRunning) {
+                m_EngineRunning = true;
+            }
+            return true;
+        };
+        bool EngineStop() {
+            if(m_EngineRunning) {
+                m_EngineRunning = false;
+            }
+            return false;
+        };
+        bool get_EngineStatus() {
+            return m_EngineRunning;
+        };
+};
 class Car {
     protected:
-    string m_DriveMode;
+        string m_DriveMode;
     
     private:
-    int m_price;
-    int m_MaxSeating;
-    void m_UpdatePrice(int base = 500000) {
-        m_price = m_MaxSeating * base;
-    }
+        Engine m_Engine;
+        int m_price;
+        int m_MaxSeating;
+        void m_UpdatePrice(int base = 500000) {
+            m_price = m_MaxSeating * base;
+        }
     public:
-    string m_brand;
-    string m_model;
-    int m_year;
-    
-    Car(string x, string y, int z, int s) {
-        m_brand = x;
-        m_model = y;
-        m_year = z;
-        m_MaxSeating = s;
-        m_UpdatePrice();
-        m_DriveMode = "No-wheel";
-    }
-    int get_m_MaxSeating() {
-        return m_MaxSeating;
-    }
-    int get_m_price() {
-        return m_MaxSeating * 500000;
-    }
-    string get_DriveMode() {
-        return m_DriveMode;
-    }
+        string m_brand;
+        string m_model;
+        int m_year;
+        
+        Car(string x, string y, int z, int s) {
+            m_brand = x;
+            m_model = y;
+            m_year = z;
+            m_MaxSeating = s;
+            m_UpdatePrice();
+            m_DriveMode = "No-wheel";
+        }
+        int get_m_MaxSeating() {
+            return m_MaxSeating;
+        }
+        int get_m_price() {
+            return m_MaxSeating * 500000;
+        }
+        string get_DriveMode() {
+            return m_DriveMode;
+        }
+        bool get_EngineStatus() {
+            return m_Engine.get_EngineStatus();
+        };
+        bool startEngine() {
+            return m_Engine.EngineStart();
+        };
+        bool stopEngine() {
+            return m_Engine.EngineStop();
+        };
 };
 
-class BMW_Car : public Car {
-   
-    
+class BMW_Car : public Car {   
     public:
     BMW_Car(string y, int z, int s) : Car("BMW", y, z, s) {
         cout << "Constructing BMW_Car\n";
@@ -48,9 +85,7 @@ class BMW_Car : public Car {
         return m_DriveMode;
     }
 };
-class AUDI_Car : public Car {
-    
-    
+class AUDI_Car : public Car {   
     public:
     AUDI_Car(string y, int z, int s) : Car("AUDI", y, z, s) {
         cout << "Constructing AUDI_Car\n";
@@ -60,18 +95,32 @@ class AUDI_Car : public Car {
         return m_DriveMode;
     }
 };
+class BENZ_Car: public Car {
+    public:
+    BENZ_Car(string y, int z, int s) : Car("BENZ", y, z, s) {
+        cout << "Constructing BENZ_Car" << endl;
+        m_DriveMode = "Rear-wheel";
+    }
+    string get_DriveMode() {
+        return m_DriveMode;
+    }
+};
+
+
 
 int main()
 {
-    Car car_0("CGU", "CSIE", 2023, 4); 
-    cout << car_0.m_brand;
-    cout << " Drive Mode = " << car_0.get_DriveMode() << endl;
+
     BMW_Car car_1("X5", 2023, 6);
     cout << car_1.m_brand;
-    cout << " : Drive Mode = " << car_1.get_DriveMode() << endl;
+    cout << " : EngineStatus = " << car_1.get_EngineStatus() << endl;
     
     AUDI_Car car_2("A1", 2023, 5);
+    car_2.startEngine();
     cout << car_2.m_brand;
-    cout << " : Drive Mode = " << car_2.get_DriveMode() << endl;
+    cout << " : EngineStatus = " << car_2.get_EngineStatus() << endl;
+    car_2.stopEngine();
+    cout << " : EngineStatus = " << car_2.get_EngineStatus() << endl;
+
     return 0;
 }
